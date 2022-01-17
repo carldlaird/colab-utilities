0d8e0a2c19d578a75983fa74767d9b1bb63e840f
Carl Laird <claird@users.noreply.github.com>
2022-01-16 22:01:03 -0500

    adding reference to Jeff Kantors page with more information

--- utils/06462/install_packages_06462.py
+++ utils/06462/install_packages_06462.py
@@ -1,3 +1,7 @@
+# This installation script implemented based on the notebook from Jeff Kantor
+# see that notebook for more information, examples, and testing
+# https://colab.research.google.com/github/jckantor/ND-Pyomo-Cookbook/blob/master/notebooks/01.02-Running-Pyomo-on-Google-Colab.ipynb#scrollTo=RlmSEN45glrk
+
 import os
 os.system('apt-get install -y -qq glpk-utils')
 os.system('apt-get install -y -qq coinor-cbc')
