55e030a05b4cb5b933e85efd6e67382dc1f4716a
Carl Laird <carl.d.laird@users.noreply.github.com>
2022-01-17 11:53:52 -0500

    adding calling instructions

--- utils/06462/install_packages_06462.py
+++ utils/06462/install_packages_06462.py
@@ -1,6 +1,9 @@
 # This installation script implemented based on the notebook from Jeff Kantor
 # see that notebook for more information, examples, and testing
 # https://colab.research.google.com/github/jckantor/ND-Pyomo-Cookbook/blob/master/notebooks/01.02-Running-Pyomo-on-Google-Colab.ipynb#scrollTo=RlmSEN45glrk
+# execute this script in your google colab with the following lines
+# >>> !wget https://raw.githubusercontent.com/carldlaird/colab-utilities/main/utils/06462/install_packages_06462.py
+# >>> import install_packages_06462
 
 import os
 os.system('apt-get install -y -qq glpk-utils')
